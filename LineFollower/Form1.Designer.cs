2cfd6784f2bbd55298cca70661da92ebacf1ed72
Jarred Rudolph <jarred.rudolph@gmail.com>
2025-09-30 22:12:41 +0800

    Add project files.

---
+++ LineFollower/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace LineFollower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.getIOtimer = new System.Windows.Forms.Timer(this.components);
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            this.labelCalib = new System.Windows.Forms.Label();
+            this.buttonCalibrateWhite = new System.Windows.Forms.Button();
+            this.buttonCalibrateBlack = new System.Windows.Forms.Button();
+            this.buttonBBRev = new System.Windows.Forms.Button();
+            this.buttonBBFwd = new System.Windows.Forms.Button();
+            this.labelMode = new System.Windows.Forms.Label();
+            this.buttonPIDFwd = new System.Windows.Forms.Button();
+            this.buttonPIDRev = new System.Windows.Forms.Button();
+            this.maskedTextBoxSensorLeft = new System.Windows.Forms.MaskedTextBox();
+            this.labelSensorLeft = new System.Windows.Forms.Label();
+            this.labelSensorRight = new System.Windows.Forms.Label();
+            this.maskedTextBoxSensorRight = new System.Windows.Forms.MaskedTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripSerialLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripSerialStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.serial = new System.IO.Ports.SerialPort(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // getIOtimer
+            // 
+            this.getIOtimer.Enabled = true;
+            this.getIOtimer.Interval = 5;
+            this.getIOtimer.Tick += new System.EventHandler(this.getIOtimer_Tick);
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.BackColor = System.Drawing.Color.Red;
+            this.buttonStartStop.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStartStop.Location = new System.Drawing.Point(20, 10);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(107, 40);
+            this.buttonStartStop.TabIndex = 0;
+            this.buttonStartStop.Text = "Run / Stop";
+            this.buttonStartStop.UseVisualStyleBackColor = false;
+            this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click);
+            // 
+            // labelCalib
+            // 
+            this.labelCalib.AutoSize = true;
+            this.labelCalib.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.labelCalib.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCalib.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelCalib.Location = new System.Drawing.Point(2, 84);
+            this.labelCalib.Name = "labelCalib";
+            this.labelCalib.Size = new System.Drawing.Size(150, 21);
+            this.labelCalib.TabIndex = 1;
+            this.labelCalib.Text = "Sensor Calibration";
+            // 
+            // buttonCalibrateWhite
+            // 
+            this.buttonCalibrateWhite.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalibrateWhite.Location = new System.Drawing.Point(19, 105);
+            this.buttonCalibrateWhite.Name = "buttonCalibrateWhite";
+            this.buttonCalibrateWhite.Size = new System.Drawing.Size(107, 38);
+            this.buttonCalibrateWhite.TabIndex = 2;
+            this.buttonCalibrateWhite.Text = "White Surface";
+            this.buttonCalibrateWhite.UseVisualStyleBackColor = true;
+            this.buttonCalibrateWhite.Click += new System.EventHandler(this.buttonCalibrateWhite_Click);
+            // 
+            // buttonCalibrateBlack
+            // 
+            this.buttonCalibrateBlack.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalibrateBlack.Location = new System.Drawing.Point(19, 148);
+            this.buttonCalibrateBlack.Name = "buttonCalibrateBlack";
+            this.buttonCalibrateBlack.Size = new System.Drawing.Size(108, 38);
+            this.buttonCalibrateBlack.TabIndex = 3;
+            this.buttonCalibrateBlack.Text = "Black Surface";
+            this.buttonCalibrateBlack.UseVisualStyleBackColor = true;
+            this.buttonCalibrateBlack.Click += new System.EventHandler(this.buttonCalibrateBlack_Click);
+            // 
+            // buttonBBRev
+            // 
+            this.buttonBBRev.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBBRev.Location = new System.Drawing.Point(198, 148);
+            this.buttonBBRev.Name = "buttonBBRev";
+            this.buttonBBRev.Size = new System.Drawing.Size(108, 38);
+            this.buttonBBRev.TabIndex = 6;
+            this.buttonBBRev.Text = "Bang-Bang Reverse";
+            this.buttonBBRev.UseVisualStyleBackColor = true;
+            this.buttonBBRev.Click += new System.EventHandler(this.buttonBBRev_Click);
+            // 
+            // buttonBBFwd
+            // 
+            this.buttonBBFwd.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBBFwd.Location = new System.Drawing.Point(199, 105);
+            this.buttonBBFwd.Name = "buttonBBFwd";
+            this.buttonBBFwd.Size = new System.Drawing.Size(107, 38);
+            this.buttonBBFwd.TabIndex = 5;
+            this.buttonBBFwd.Text = "Bang-Bang Forward";
+            this.buttonBBFwd.UseVisualStyleBackColor = true;
+            this.buttonBBFwd.Click += new System.EventHandler(this.buttonBBFwd_Click);
+            // 
+            // labelMode
+            // 
+            this.labelMode.AutoSize = true;
+            this.labelMode.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.labelMode.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMode.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelMode.Location = new System.Drawing.Point(202, 84);
+            this.labelMode.Name = "labelMode";
+            this.labelMode.Size = new System.Drawing.Size(104, 21);
+            this.labelMode.TabIndex = 4;
+            this.labelMode.Text = "Mode Select";
+            // 
+            // buttonPIDFwd
+            // 
+            this.buttonPIDFwd.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPIDFwd.Location = new System.Drawing.Point(199, 192);
+            this.buttonPIDFwd.Name = "buttonPIDFwd";
+            this.buttonPIDFwd.Size = new System.Drawing.Size(108, 38);
+            this.buttonPIDFwd.TabIndex = 7;
+            this.buttonPIDFwd.Text = "PID Forward";
+            this.buttonPIDFwd.UseVisualStyleBackColor = true;
+            this.buttonPIDFwd.Click += new System.EventHandler(this.buttonPIDFwd_Click);
+            // 
+            // buttonPIDRev
+            // 
+            this.buttonPIDRev.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPIDRev.Location = new System.Drawing.Point(198, 235);
+            this.buttonPIDRev.Name = "buttonPIDRev";
+            this.buttonPIDRev.Size = new System.Drawing.Size(108, 38);
+            this.buttonPIDRev.TabIndex = 8;
+            this.buttonPIDRev.Text = "PID Reverse";
+            this.buttonPIDRev.UseVisualStyleBackColor = true;
+            this.buttonPIDRev.Click += new System.EventHandler(this.buttonPIDRev_Click);
+            // 
+            // maskedTextBoxSensorLeft
+            // 
+            this.maskedTextBoxSensorLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.maskedTextBoxSensorLeft.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.maskedTextBoxSensorLeft.Location = new System.Drawing.Point(241, 10);
+            this.maskedTextBoxSensorLeft.Name = "maskedTextBoxSensorLeft";
+            this.maskedTextBoxSensorLeft.ReadOnly = true;
+            this.maskedTextBoxSensorLeft.Size = new System.Drawing.Size(65, 23);
+            this.maskedTextBoxSensorLeft.TabIndex = 9;
+            // 
+            // labelSensorLeft
+            // 
+            this.labelSensorLeft.AutoSize = true;
+            this.labelSensorLeft.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSensorLeft.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelSensorLeft.Location = new System.Drawing.Point(145, 10);
+            this.labelSensorLeft.Name = "labelSensorLeft";
+            this.labelSensorLeft.Size = new System.Drawing.Size(94, 21);
+            this.labelSensorLeft.TabIndex = 10;
+            this.labelSensorLeft.Text = "Left Sensor";
+            // 
+            // labelSensorRight
+            // 
+            this.labelSensorRight.AutoSize = true;
+            this.labelSensorRight.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSensorRight.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelSensorRight.Location = new System.Drawing.Point(133, 37);
+            this.labelSensorRight.Name = "labelSensorRight";
+            this.labelSensorRight.Size = new System.Drawing.Size(106, 21);
+            this.labelSensorRight.TabIndex = 11;
+            this.labelSensorRight.Text = "Right Sensor";
+            // 
+            // maskedTextBoxSensorRight
+            // 
+            this.maskedTextBoxSensorRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.maskedTextBoxSensorRight.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.maskedTextBoxSensorRight.Location = new System.Drawing.Point(241, 37);
+            this.maskedTextBoxSensorRight.Name = "maskedTextBoxSensorRight";
+            this.maskedTextBoxSensorRight.Size = new System.Drawing.Size(65, 23);
+            this.maskedTextBoxSensorRight.TabIndex = 12;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSerialLabel,
+            this.toolStripSerialStatus,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 357);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 12, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(375, 22);
+            this.statusStrip1.TabIndex = 13;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripSerialLabel
+            // 
+            this.toolStripSerialLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripSerialLabel.BorderStyle = System.Windows.Forms.Border3DStyle.RaisedOuter;
+            this.toolStripSerialLabel.Name = "toolStripSerialLabel";
+            this.toolStripSerialLabel.Size = new System.Drawing.Size(79, 17);
+            this.toolStripSerialLabel.Text = "Serial Status : ";
+            // 
+            // toolStripSerialStatus
+            // 
+            this.toolStripSerialStatus.Name = "toolStripSerialStatus";
+            this.toolStripSerialStatus.Size = new System.Drawing.Size(112, 17);
+            this.toolStripSerialStatus.Text = "toolStripSerialStatus";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(375, 379);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.maskedTextBoxSensorRight);
+            this.Controls.Add(this.labelSensorRight);
+            this.Controls.Add(this.labelSensorLeft);
+            this.Controls.Add(this.maskedTextBoxSensorLeft);
+            this.Controls.Add(this.buttonPIDRev);
+            this.Controls.Add(this.buttonPIDFwd);
+            this.Controls.Add(this.buttonBBRev);
+            this.Controls.Add(this.buttonBBFwd);
+            this.Controls.Add(this.labelMode);
+            this.Controls.Add(this.buttonCalibrateBlack);
+            this.Controls.Add(this.buttonCalibrateWhite);
+            this.Controls.Add(this.labelCalib);
+            this.Controls.Add(this.buttonStartStop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Line Following Robot";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer getIOtimer;
+        private Button buttonStartStop;
+        private Label labelCalib;
+        private Button buttonCalibrateWhite;
+        private Button buttonCalibrateBlack;
+        private Button buttonBBRev;
+        private Button buttonBBFwd;
+        private Label labelMode;
+        private Button buttonPIDFwd;
+        private Button buttonPIDRev;
+        private MaskedTextBox maskedTextBoxSensorLeft;
+        private Label labelSensorLeft;
+        private Label labelSensorRight;
+        private MaskedTextBox maskedTextBoxSensorRight;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripSerialLabel;
+        private ToolStripStatusLabel toolStripSerialStatus;
+        private System.IO.Ports.SerialPort serial;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ LineFollower/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LineFollower
+{
+    public partial class Form1 : Form
+    {
+        bool runRobot = false, runSerial = true;
+        int input1 = 0, input2 = 0, count = 0, controlMode = 0;
+        double curTime = 0, prevTime = 0, totTime = 0;
+        byte leftSensor, rightSensor;
+        private Stopwatch stopwatch = new Stopwatch();
+
+        bool run = false;
+
+        byte[] outputs = new byte[4];
+        byte[] inputs = new byte[4];
+
+        const byte START = 255;
+        const byte ZERO = 0;
+        const int LOWER_DEADBAND = 1;
+        const int LEFT_SENSOR = 0;
+        const int RIGHT_SENSOR = 1;
+        const int LEFT_MOTOR = 2;
+        const int RIGHT_MOTOR = 3;
+        const int SENSOR_MIN = 0;
+        const int SENSOR_MAX = 255;
+        const int MAX_FWD = 75;
+        const int MAX_REV = 25;
+        const int MOTOR_STOP = 50;
+        const int BB_FWD = 0;
+        const int BB_REV = 1;
+        const int PID_FWD = 2;
+        const int PID_REV = 3;
+        const float DUTY_STEP = (float)244 / (float)100;
+        const float CENTER = (float)0.05;
+
+        public Form1()
+        {
+            InitializeComponent();
+            stopwatch.Start();
+            
+
+            if (runSerial)
+            {
+                if (!serial.IsOpen)
+                {
+                    try
+                    {
+                        serial.Open();
+                    }
+                    catch
+                    {
+                        toolStripSerialStatus.Text = "Error: Failed to connect.";
+                    }
+                }
+            }
+
+
+        }
+
+        // Main loop run at end of timer tick.
+        private void loop()
+        {
+            curTime = stopwatch.ElapsedMilliseconds;
+            totTime = stopwatch.Elapsed.TotalSeconds;
+            toolStripStatusLabel1.Text = totTime.ToString();
+            if (!runRobot)
+            {
+                MotorDrive(MOTOR_STOP, MOTOR_STOP);
+            }
+            else
+            {
+                switch (controlMode)
+                {
+                    case BB_FWD:
+                        BBFwd();
+                        break;
+                    case BB_REV:
+                        BBRev();
+                        break;
+                    case PID_FWD:
+
+                        break;
+                    case PID_REV:
+
+                        break;
+                }
+            }
+        }
+
+        private void BBFwd()
+        {
+            float position = FindPosition(leftSensor, rightSensor);
+
+            if (position >-CENTER && position < CENTER)
+            {
+                position = 0;
+            }
+
+            if (position < 0)
+            {
+                MotorDrive(MAX_FWD, 40);
+            }
+            else if (position > 0)
+            {
+                MotorDrive(40, MAX_FWD);
+            }
+            else
+            {
+                MotorDrive(MAX_FWD, MAX_FWD);
+            }
+        }
+
+        private void BBRev()
+        {
+            float position = FindPosition(leftSensor, rightSensor);
+
+            if (position > -CENTER && position < CENTER)
+            {
+                position = 0;
+            }
+
+            if (position > 0)
+            {
+                MotorDrive(MAX_REV, 60);
+            }
+            else if (position < 0)
+            {
+                MotorDrive(60, MAX_REV);
+            }
+            else
+            {
+                MotorDrive(MAX_REV, MAX_REV);
+            }
+        }
+
+        // Send a four byte message to the Arduino via serial.
+        private void SendIO(byte PORT, byte DATA)
+        {
+            outputs[0] = START; // Set the first byte to the start value that indicated the beginning of the message.
+            outputs[1] = PORT;  // Set the second byte to represend the port where, leftSensor = 0, rightSensor = 1, leftMotor = 2, rightMotor = 3.
+            outputs[2] = DATA;  // Set the third data byte to the value to be assigned to the port. 
+            outputs[3] = (byte)(START + PORT + DATA); // Calculate the checksum byte.
+
+            if (serial.IsOpen)
+            {
+                serial.Write(outputs, 0, 4); // Send all four bytes to the IO Card.
+            }
+        }
+
+        // Find the position of the vehicle with relation to the line.
+        private float FindPosition(byte leftSensor, byte rightSensor)
+        {
+            float difference = leftSensor - rightSensor;
+            float position = difference / SENSOR_MAX; // -1 when left of line, 0 when center on line, 1 when right of line
+
+            return position;
+        }
+
+        // Convert the commanded duty cycle to a bit output.
+        private byte ConvertDutyCycle(float inDuty)
+        {
+            float result;
+            if (inDuty < LOWER_DEADBAND) // For duty cycles commanded below 1%
+            {
+                result = ZERO;
+            }
+            else
+            {
+                result = DUTY_STEP * inDuty;
+            }
+            result = (float)Math.Floor(result);
+            return (byte)result;
+        }
+
+        // Command motors to required duty cycle.
+        private void MotorDrive(float leftDuty, float rightDuty)
+        {
+            SendIO(LEFT_MOTOR, ConvertDutyCycle(leftDuty));
+            SendIO(RIGHT_MOTOR, ConvertDutyCycle(rightDuty));
+        }
+
+        private void buttonStartStop_Click(object sender, EventArgs e)
+        {
+            if (!runRobot)
+            {
+                runRobot = true;
+                buttonStartStop.BackColor = Color.Green;
+            }
+            else
+            {
+                runRobot = false;
+                buttonStartStop.BackColor = Color.Red;
+            }
+        }
+
+        private void getIOtimer_Tick(object sender, EventArgs e)
+        {
+            if (serial.IsOpen)
+            {
+                SendIO((byte)count, ZERO); // Trigger sensor read, cycle between sensors every timer tick
+                if (count == LEFT_SENSOR)
+                {
+                    count = RIGHT_SENSOR;
+                }
+                else {
+                    count = LEFT_SENSOR;
+                }
+            
+                if (serial.BytesToRead >= 4) // Check that the buffer contains a full four byte package.
+                {
+                    //toolStripSerialStatus.Text = "Incoming";
+                    inputs[0] = (byte)serial.ReadByte(); // Read the first byte of the package.
+
+                    // Check that the first byte is in fact the start byte.
+                    if (inputs[0] == START)
+                    {
+                        //toolStripSerialStatus.Text = "Start Accepted";
+
+                        // Read the rest of the package.
+                        inputs[1] = (byte)serial.ReadByte();
+                        inputs[2] = (byte)serial.ReadByte();
+                        inputs[3] = (byte)serial.ReadByte();
+
+                        // Claculate the checksum.
+                        byte checkSum = (byte)(inputs[0] + inputs[1] + inputs[2]);
+
+                        // Check that the calculated check sum matches the check sum sent with the message.
+                        if (inputs[3] == checkSum)
+                        {
+                            //toolStripSerialStatus.Text = "CheckSum Accepted";
+
+                            // Check which port the incoming data is associated with.
+                            switch (inputs[1])
+                            {
+                                case LEFT_SENSOR:
+                                    //toolStripSerialStatus.Text = "Left Sensor Read";
+                                    leftSensor = inputs[2];
+                                    maskedTextBoxSensorLeft.Text = leftSensor.ToString();
+                                    break;
+                                case RIGHT_SENSOR:
+                                    //toolStripSerialStatus.Text = "Right Sensor Read";
+                                    rightSensor = inputs[2];
+                                    maskedTextBoxSensorRight.Text = rightSensor.ToString();
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            loop();
+        }
+
+        private void buttonBBFwd_Click(object sender, EventArgs e)
+        {
+            controlMode = BB_FWD;
+            buttonBBFwd.BackColor = Color.FromArgb(204, 255, 255, 1);
+            buttonBBRev.BackColor = SystemColors.ControlLightLight;
+            buttonPIDFwd.BackColor = SystemColors.ControlLightLight;
+            buttonPIDRev.BackColor = SystemColors.ControlLightLight;
+        }
+
+        private void buttonBBRev_Click(object sender, EventArgs e)
+        {
+            controlMode = BB_REV;
+            buttonBBFwd.BackColor = SystemColors.ControlLightLight;
+            buttonBBRev.BackColor = Color.FromArgb(204, 255, 255, 1);
+            buttonPIDFwd.BackColor = SystemColors.ControlLightLight;
+            buttonPIDRev.BackColor = SystemColors.ControlLightLight;
+        }
+
+        private void buttonPIDFwd_Click(object sender, EventArgs e)
+        {
+            controlMode = PID_FWD;
+            buttonBBFwd.BackColor = SystemColors.ControlLightLight;
+            buttonBBRev.BackColor = SystemColors.ControlLightLight;
+            buttonPIDFwd.BackColor = Color.FromArgb(204, 255, 255, 1);
+            buttonPIDRev.BackColor = SystemColors.ControlLightLight;
+        }
+
+        private void buttonPIDRev_Click(object sender, EventArgs e)
+        {
+            controlMode = PID_REV;
+            buttonBBFwd.BackColor = SystemColors.ControlLightLight;
+            buttonBBRev.BackColor = SystemColors.ControlLightLight;
+            buttonPIDFwd.BackColor = SystemColors.ControlLightLight;
+            buttonPIDRev.BackColor = Color.FromArgb(204, 255, 255, 1);
+        }
+
+        private void buttonCalibrateWhite_Click(object sender, EventArgs e)
+        {
+            buttonCalibrateWhite.BackColor = Color.Green;
+        }
+
+        private void buttonCalibrateBlack_Click(object sender, EventArgs e)
+        {
+            buttonCalibrateBlack.BackColor = Color.Green;
+        }
+    }
+}
